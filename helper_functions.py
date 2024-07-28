ef9fd978968d18848d8499399c5d682d19bbbbec
GANESH GAIKWAD <113162121+KenKaneki2207@users.noreply.github.com>
2024-07-28 15:24:13 +0530

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -10,3 +10,36 @@ def walk_directory(path):
 
   for dirpath, dirnames, filenames in os.walk(path):
     print(f"There are {len(dirnames)} directories and {len(filenames)} images in {dirpath}")
+
+
+import random
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+import os
+
+def random_images(path, class_name=None):
+  '''
+  Prerequisite Libraries: os, random, matplotlib.pyplot and  matplotlib.image
+  ""import random
+    import matplotlib.pyplot as plt
+    from matplotlib.image import imread
+    import os""
+  
+
+  Input : path --> path to the directory,
+          class_name = None(by default), can provide the name of a specific directory in given path.
+
+  Output : random iamge from the path directory.
+
+  '''
+
+  if class_name == None:
+    classes = random.choice(os.listdir(path))
+  else:
+    classes = class_name
+  img = random.choice(os.listdir(f"{path}/{classes}"))
+  title = classes
+
+  plt.imshow(imread(f"{path}/{title}/{img}"))
+  plt.axis("off")
+  plt.title(title)
