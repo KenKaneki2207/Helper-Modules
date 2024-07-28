2e1b847f399d74d8bba64d9829e6ed4667f3dc1a
GANESH GAIKWAD <113162121+KenKaneki2207@users.noreply.github.com>
2024-07-28 15:09:05 +0530

    Create helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,12 @@
+def walk_directory(path):
+  '''
+  Prerequisities Libraries : import os, before using this function.
+
+  Input : path --> path to the directory
+
+  Output : All the sub-directories and filenames in the path provided.
+  
+  '''
+
+  for dirpath, dirnames, filenames in os.walk(path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in {dirpath}")
